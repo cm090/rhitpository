06c6cbeaf3b8917f02455bd2ea9e23985e53f858
Canon Maranda <22649817+cm090@users.noreply.github.com>
2022-06-27 13:36:46 -0500

    Upload MA221 files

--- site.config.ts
+++ site.config.ts
@@ -42,11 +42,11 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/chem/chem111': '9cb622b6601d403fafb92e12798a1cbd',
+    '/csse/csse132': 'ee5ad2a1b1614f8294b527a12d5dab6c'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
